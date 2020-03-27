d4aef8353fb368f81972a5bd3dac840677ebf093
bchansen <bchansen@live.com>
2020-03-26 20:50:02 -0600

    schemas for users and modification to inventory schema to include user id

--- models/user.js
+++ models/user.js
@@ -1,2 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    userId: {
+        type: Integer,
+        required: true
+    },
+    inventoryOwned: {
+        itemId: {
+            type: Integer,
+            required: true
+        }
+    },
+    inventoryRented: {
+        itemId: {
+            type: Integer,
+            required: true
+        },
+        checkoutDate: {
+            type: Date
+        },
+        checkinDate: {
+            type: Date
+        }
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = userSchema;
