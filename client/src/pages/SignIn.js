d20f7427df52ac8943e6a37d3a880e1a61b0ccb9
bchansen <bchansen@live.com>
2020-03-26 20:52:09 -0600

    Added pages for sign in and inventory

---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import HomeScreen from "../components/HomeScreen";
+import Nav from "../components/Nav";
+
+class SignIn extends Component {
+
+
+
+    render () {
+        return (
+            
+        <React.Fragment>
+            <HomeScreen />
+      </React.Fragment>
+        );
+    }
+}
+
+export default SignIn;
