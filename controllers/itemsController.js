acc8f289a18797d46b4bf53dbc292a165c8621dc
bchansen <bchansen@live.com>
2020-04-03 17:32:33 -0600

    Stashing

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -17,13 +17,23 @@ module.exports = {
     homeSearch: function (req, res) {
         console.log(req.body);
         db.Inventory
-        .find({ title: { $regex: req.body.params.search }})
+        .find({ title: { $regex: req.body.params.search}, city: { $regex: req.body.params.city }})
         .then(dbModel => {
             res.json(dbModel)
         })
         .catch(err => {
             console.log(err)
             res.status(422).json(err)
+        })
+    },
+    singleItem: function(req, res) {
+        db.Inventory
+        .find({itemId: req.body.params.item})
+        .then(dbModel => {
+            res.json(dbModel)
+        }).catch(err => {
+            console.log(err)
+            res.status(407).json(err)
         })
     }
 }
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const itemsController = require("../../controllers/itemsController");
 
 router.route("/api/inventory/search")
-.post(itemsController.homeSearch);
+.post(itemsController.homeSearch); 
 
 router.route("/api/inventory/:id")
 .get(itemsController.inventorySearch);
 
-
+router.route("/api/item")
+.get(itemsController.singleItem);
 
 module.exports = router;
